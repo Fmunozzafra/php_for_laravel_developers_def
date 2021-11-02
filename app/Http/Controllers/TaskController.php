4f3fbabd83efa6748060e592dae61068fb87d76a
Fmunozzafra <fmunoz@iesebre.com>
2021-11-02 19:34:31 +0100

    Refactoritzar la funció anonima que tenia a routes/web.php a un controlador

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    // CRUD ->
+    // CREATE -> create (formulari) -> store POST
+    // RETRIEVE -> LIST (index) i SHOW (mostra un item)
+    // UPDATE -> edit (formulari) -> update -> POST del formulari edició
+    // DELETE -> destroy
+    // REST API -> tubeme.acacha.org/http
+    public function index()
+    {
+        return view('tasks',[
+            'tasks' => Task::all()
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,41 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    $task1 = new stdClass();
-    $task1->id = 1;
-    $task1->title = "Task 1";
-    $task1->description = "Test";
-    $task1->completed = 1;
-    $task2 = new stdClass();
-    $task2->id = 2;
-    $task2->title = "Task 2";
-    $task2->description = "Test";
-    $task2->completed = 1;
+Route::get('/', [TaskController::class,'index']);
 
-    $task3 = new stdClass();
-    $task3->id = 3;
-    $task3->title = "Task 3";
-    $task3->description = "Test";
-    $task3->completed = 0;
-
-    $tasks = [
-        $task1,
-        $task2,
-        $task3
-    ];
-
-    return view('tasks',[
-        'tasks' => $tasks,
-        'users' => $user
-    ]);
-});
-
-Route::get('/tasks', function () {
-    return view('tasks', [
-        'tasks' => []
-    ]);
-});
+Route::get('/tasks', [TaskController::class,'index']);
 
 Route::get('/users', function () {
     return view('users');
